8d4574578f9754863c3cce9321f9c61cc6d01adb
luckyhattimare07 <104069948+luckyhattimare07@users.noreply.github.com>
2025-04-15 10:20:04 +0530

    Time: 5 ms (71.59%), Space: 44.6 MB (7.92%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.java
+++ 0009-palindrome-number/0009-palindrome-number.java
@@ -1,16 +1,13 @@
 class Solution {
     public boolean isPalindrome(int x) {
-                String s = String.valueOf(x); // Convert to String
-        int n = s.length(); // Store the String length to int n
-
-        for (int i=0; i<n/2; i++) {
-            // We check whether the elements at the same distance from
-            // beginning and from ending are same, if not we return false
-            if (s.charAt(i) != s.charAt(n-i-1)) return false;
+         if (x < 0 || (x % 10 == 0 && x != 0)) {
+            return false;
         }
-
-        // if no flaws are found we return true
-        return true;
-
+        int revertedNumber = 0;
+        while (x > revertedNumber) {
+            revertedNumber = revertedNumber * 10 + x % 10;
+            x /= 10;
+        }
+        return x == revertedNumber || x == revertedNumber / 10;
     }
 }
